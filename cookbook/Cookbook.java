d29061df3cc46fd7381f057b9f64958f403c4713
ni2050 <ni2050@nyu.edu>
2024-05-05 18:27:02 -0400

    basic classes

---
+++ cookbook/Cookbook.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Cookbook {
+    ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+}
+
+public static void main(String[] args){
+
+}
--- cookbook/Recipe.java
+++ cookbook/Recipe.java
@@ -1,18 +1,29 @@
 // Recipe.java
 
-class Recipe {
+import java.util.ArrayList;
+
+public class Recipe {
     private int id;
     private String name;
+    private float difficultyRatingOutOf10;
     private String description;
     private String userId;
     private String time;
+    private ArrayList<String> ingredients = new ArrayList<String>();
+    private ArrayList<String> steps = new ArrayList<String>();
+    
+    private boolean favorite;
 
-    public Recipe(int id, String name, String description, String time, String userId) {
+    public Recipe(int id, String name, float difficultyRatingOutOf10, String description, String time,
+     String userId,ArrayList<String>ingredients, ArrayList<String> steps) {
         this.id = id;
         this.name = name;
+        this.difficultyRatingOutOf10=difficultyRatingOutOf10;
         this.description = description;
         this.time = time;
         this.userId = userId;
+        this.ingredients=ingredients;
+        this.steps=steps;
     }
 
     public int getId() {
@@ -21,6 +32,10 @@ class Recipe {
 
     public String getName() {
         return name;
+    }
+
+    public float getDifficultyRatingOutOf10() {
+        return difficultyRatingOutOf10;
     }
 
     public String getDescription() {
@@ -33,5 +48,21 @@ class Recipe {
     
     public String getTime() {
         return time;
+    }
+
+    public ArrayList<String> getIngredients(){
+        return ingredients;
+    }
+
+    public ArrayList<String> getSteps(){
+        return steps;
+    }
+
+    public boolean getFavorite(){
+        return favorite;
+    }
+
+    public void setFavorite(){
+        favorite=true;
     }
 }
