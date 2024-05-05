741770dafb35ce8d0e13a60ad249bbf9d88ea58e
saragizabi <158234859+saragizabi@users.noreply.github.com>
2024-05-05 16:28:27 -0400

    Create Recipe.java

---
+++ cookbook/Recipe.java
@@ -0,0 +1,37 @@
+// Recipe.java
+
+class Recipe {
+    private int id;
+    private String name;
+    private String description;
+    private String userId;
+    private String time;
+
+    public Recipe(int id, String name, String description, String time, String userId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.time = time;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+    
+    public String getTime() {
+        return time;
+    }
+}
