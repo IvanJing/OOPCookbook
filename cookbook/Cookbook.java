e16bf5c6bd10eca0fbae250571d3dd2e4898ea9a
ni2050 <ni2050@nyu.edu>
2024-05-05 18:54:07 -0400

    ui initial

--- cookbook/Cookbook.java
+++ cookbook/Cookbook.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class Cookbook {
     ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+
+    public void deleteRecipe(Recipe r){
+        recipes.remove();
+    }
 }
 
 public static void main(String[] args){
---
+++ cookbook/UI.java
@@ -0,0 +1,16 @@
+import java.util.*
+
+public class UI {
+    Scanner scn = new Scanner(System.in);
+    
+
+    public void deleteUI(){
+        System.out.print("______________________________\n");
+        System.out.print("Delete Recipe\n");
+        System.out.println("______________________________");
+        System.out.println("How would you like to delete a recipe?");
+        System.out.println("1. By name");
+        System.out.println("2. By id");
+        int opt=scn.nextInt();
+    }
+}
